8e43f1672998a1b147d7e6a735e7be92d803ad7f
wingyiu <wyiu.lee@gmail.com>
2017-08-26 13:43:17 +0800

    Create get_proxies.sh

---
+++ bin/get_proxies.sh
@@ -0,0 +1,15 @@
+# source /root/venv/crawl/bin/activate
+
+# export SCRAPY_PROJECT=proxy
+
+
+while true; do
+    scrapy crawl proxy_ip181
+    scrapy crawl proxy_kxdaili
+    scrapy crawl proxy_66ipcn
+    scrapy crawl proxy_data5u
+    scrapy crawl proxy_xicidaili
+    scrapy crawl proxy_yaoyaodaili
+
+    sleep 10
+done
